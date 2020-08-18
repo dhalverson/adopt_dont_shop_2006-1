6c3debf4eefa0e9c013392ebcf41253b41b6af80
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-18 15:46:22 -0600

    Create user story 3 test

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -26,4 +26,15 @@ RSpec.describe "As a visitor", type: :feature do
     expect(page).to have_content(@shelter_1.name)
     expect(page).to have_content(@shelter_2.name)
   end
+  
+  it "When I visit /shelters/:id I see all information pertaining to this shelter" do
+
+    visit "/shelters/#{@shelter_1.id}"
+    
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.address)
+    expect(page).to have_content(@shelter_2.city)
+    expect(page).to have_content(@shelter_2.state)
+    expect(page).to have_content(@shelter_2.zip)
+  end
 end
