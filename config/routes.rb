8fd6632735f05de6b401e022c4d91009f1cc6a3d
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-18 20:53:50 -0600

    Added post route for creating a new shelter

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "/shelters", to: "shelters#index"
-  get "/shelters/:id", to: "shelters#show"
   get "/shelters/new", to: "shelters#new"
-
+  get "/shelters/:id", to: "shelters#show"
+  post "/shelters", to: "shelters#create"
 end
