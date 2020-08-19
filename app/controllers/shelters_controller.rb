a25b0d80a9ea493cadfe12777ebfe8cc400aa096
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-18 20:53:19 -0600

    Comment out flash message for later usage

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -16,10 +16,10 @@ class SheltersController < ApplicationController
     @new_shelter = Shelter.new(shelter_params)
     if @new_shelter.valid?
       @new_shelter.save
-      flash[:success] = "#{@new_shelter.name} has been created..."
+      # flash[:success] = "#{@new_shelter.name} has been created..."
       redirect_to "/shelters"
     else
-      flash[:error] = "#{@new_shelter.name} was not created due to missing information..."
+      # flash[:error] = "#{@new_shelter.name} was not created due to missing information..."
       render :new
     end
   end
