f0eb3408219296bd3210ca1dbabb312ed23ed704
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-20 14:54:26 -0600

    Begin user story 9. Added description column to pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_204204) do
+ActiveRecord::Schema.define(version: 2020_08_20_205249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_204204) do
     t.bigint "shelter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description", default: "nil"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
