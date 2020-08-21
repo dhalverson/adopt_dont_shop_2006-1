acd581fc7162627c586c9dca364f43a37bb41807
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-21 11:50:18 -0600

    Add user story 10 actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,26 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
   
+  def new
+    @shelter = Shelter.find(params[:id])
+  end
+  
+  def create
+    shelter = Shelter.find(params[:id])
+    new_pet = shelter.pets.create(pet_params)
+    if new_pet.valid?
+      new_pet.save
+      # flash[:success] = "#{@new_shelter.name} has been created..."
+      redirect_to "/shelters/#{shelter.id}/pets"
+    else
+      # flash[:error] = "#{@new_shelter.name} was not created due to missing information..."
+      render :new
+    end
+  end
+  
+  private
+  
+  def pet_params
+    params.permit(:image, :name, :description, :age, :sex)
+  end
 end
