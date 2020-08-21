13b908842170599415b55e682fdececf509fdfe8
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-20 18:48:19 -0600

    Added status column to pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_205249) do
+ActiveRecord::Schema.define(version: 2020_08_21_004732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_205249) do
     t.bigint "shelter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "description", default: "nil"
+    t.string "description"
+    t.string "status"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
