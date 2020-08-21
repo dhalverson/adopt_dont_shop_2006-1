4fdfdd9ca2c56ce470db0cd999df46b98afb294f
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-21 12:31:11 -0600

    Correct file naming from previous test

--- spec/features/pets/new_spec.rb
+++ spec/features/pets/new_spec.rb
@@ -19,37 +19,30 @@ RSpec.describe "As a visitor", type: :feature do
               description: "She's a 85 pound lap dog!",
               status: "Adoptable"
               )
-
   end
   
-  it "I can create new pets for a shelter" do
-    visit "/pets/#{@pet_1.id}"
-    expect(page).to have_link("Update Pet")
+  it "I can edit a shelters' pets" do
+    visit "/shelters/#{@shelter_1.id}/pets"
+    expect(page).to have_link("Create Pet")
     
+    click_on "Create Pet"
+    
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/pets/new")
+    
+    fill_in 'Image', with: @pet_1.image
+    fill_in 'Name', with: @pet_1.name
+    fill_in 'Description', with: @pet_1.description
+    fill_in 'Age', with: @pet_1.age
+    fill_in 'Sex', with: @pet_1.sex
+    
+    click_on 'Create Pet'
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/pets")
+  
     expect(page).to have_content("Name: #{@pet_1.name}")
     expect(page).to have_content("Age: #{@pet_1.age}")
     expect(page).to have_content("Sex: #{@pet_1.sex}")
     expect(page).to have_content("Description: #{@pet_1.description}")
     expect(page).to have_content("Adoption Status: #{@pet_1.status}")
-    
-    click_on "Update Pet"
-    
-    expect(current_path).to eq("/pets/#{@pet_1.id}/edit")
-    
-    fill_in 'Image', with: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*"
-    fill_in 'Name', with: "Marco"
-    fill_in 'Description', with: "Pet with updated information"
-    fill_in 'Age', with: "2"
-    fill_in 'Sex', with: "Male"
-    
-    click_on 'Update Pet'
-
-    expect(current_path).to eq("/pets/#{@pet_1.id}")
-  
-    expect(page).to have_content("Name: Marco")
-    expect(page).to have_content("Age: 2")
-    expect(page).to have_content("Sex: Male")
-    expect(page).to have_content("Description: Pet with updated information")
-    # expect(page).to have_content("Adoption Status: #{@pet_1.status}")
   end
 end
