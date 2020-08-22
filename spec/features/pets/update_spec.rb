de44e9e9c0d9a4e4be29e5976472982532362f5d
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-21 19:32:45 -0600

    Create tests for user stories 15 and 16

--- spec/features/pets/update_spec.rb
+++ spec/features/pets/update_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "As a visitor", type: :feature do
 
   end
   
-  it "I can create new pets for a shelter" do
+  it "I can update pet information for a shelter" do
     visit "/pets/#{@pet_1.id}"
     expect(page).to have_link("Update Pet")
     
@@ -50,6 +50,67 @@ RSpec.describe "As a visitor", type: :feature do
     expect(page).to have_content("Age: 2")
     expect(page).to have_content("Sex: Male")
     expect(page).to have_content("Description: Pet with updated information")
-    # expect(page).to have_content("Adoption Status: #{@pet_1.status}")
+  end
+  
+  it "From /pets I can update pet information for a shelter" do
+    visit "/pets"
+    
+    expect(page).to have_link("Update Pet")
+    save_and_open_page
+    expect(page).to have_content("Name: #{@pet_1.name}")
+    expect(page).to have_content("Age: #{@pet_1.age}")
+    expect(page).to have_content("Sex: #{@pet_1.sex}")
+    expect(page).to have_content("Description: #{@pet_1.description}")
+    expect(page).to have_content("Adoption Status: #{@pet_1.status}")
+    
+    click_on "Update Pet"
+    
+    expect(current_path).to eq("/pets/#{@pet_1.id}/edit")
+    
+    fill_in 'Image', with: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*"
+    fill_in 'Name', with: "Marco"
+    fill_in 'Description', with: "Pet with updated information"
+    fill_in 'Age', with: "2"
+    fill_in 'Sex', with: "Male"
+    
+    click_on 'Update Pet'
+
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
+  
+    expect(page).to have_content("Name: Marco")
+    expect(page).to have_content("Age: 2")
+    expect(page).to have_content("Sex: Male")
+    expect(page).to have_content("Description: Pet with updated information")
+  end
+  
+  it "From /shelters/shelter_id/pets I can update pet information for a shelter" do
+    visit "/shelters/#{@shelter_1.id}/pets"
+    
+    expect(page).to have_link("Update Pet")
+
+    expect(page).to have_content("Name: #{@pet_1.name}")
+    expect(page).to have_content("Age: #{@pet_1.age}")
+    expect(page).to have_content("Sex: #{@pet_1.sex}")
+    expect(page).to have_content("Description: #{@pet_1.description}")
+    expect(page).to have_content("Adoption Status: #{@pet_1.status}")
+    
+    click_on "Update Pet"
+    
+    expect(current_path).to eq("/pets/#{@pet_1.id}/edit")
+    
+    fill_in 'Image', with: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*"
+    fill_in 'Name', with: "Marco"
+    fill_in 'Description', with: "Pet with updated information"
+    fill_in 'Age', with: "2"
+    fill_in 'Sex', with: "Male"
+    
+    click_on 'Update Pet'
+
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
+  
+    expect(page).to have_content("Name: Marco")
+    expect(page).to have_content("Age: 2")
+    expect(page).to have_content("Sex: Male")
+    expect(page).to have_content("Description: Pet with updated information")
   end
 end
