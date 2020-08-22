569b9a6e6a6462c4b166d2b68583a75a56d4e3d9
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-21 19:14:56 -0600

    Create user story 13 test

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe "As a visitor", type: :feature do
   end
   
   it "When I visit /shelters/:id, there is a link that takes me to a form to update/edit this shelter's information" do
-
+    
     visit "/shelters/#{@shelter_1.id}"
     
     expect(page).to have_content(@shelter_1.name)
@@ -38,6 +38,32 @@ RSpec.describe "As a visitor", type: :feature do
     expect(page).to have_link("Update Shelter")
     
     click_link "Update Shelter"
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+
+    fill_in 'Name', with: 'The Updated Shelter'
+    fill_in 'Address', with: '12 Main St.'
+    fill_in 'City', with: 'Casper'
+    fill_in 'State', with: 'WY'
+    fill_in 'Zip', with: '33221'
+    
+    click_on 'Update Shelter'
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+    
+    expect(page).to have_content("The Updated Shelter")
+    expect(page).to have_content("12 Main St.")
+    expect(page).to have_content("Casper")
+    expect(page).to have_content("WY")
+    expect(page).to have_content("33221")
+
+  end
+  
+  it "When I visit /shelters, each shelter has a link that takes me to a form to update/edit this shelter's information" do
+    
+    visit "/shelters"
+    
+    click_link("Update Shelter", match: :first)
 
     expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
 
