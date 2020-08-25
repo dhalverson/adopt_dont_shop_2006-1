0c13f936084fec4dcf58460acb1772c74c58f241
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-25 12:41:58 -0600

    Added reviews/new and reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get "/shelters/:id/edit", to: "shelters#edit"
   patch "/shelters/:id", to: "shelters#update"
   delete "/shelters/:id", to: "shelters#destroy"
-  
+
   get "/shelters/:id/pets", to: "shelter_pets#index"
-  
+
   get "/pets", to: "pets#index"
   get "/pets/:id", to: "pets#show"
   get "/shelters/:id/pets/new", to: "pets#new"
@@ -17,4 +17,7 @@ Rails.application.routes.draw do
   get "/pets/:id/edit", to: "pets#edit"
   post "/pets/:id", to: "pets#update"
   delete "/pets/:id", to: "pets#destroy"
+
+  get "/shelters/:shelter_id/reviews/new", to: "shelter_reviews#new"
+  post "/shelters/:shelter_id/reviews", to: "shelter_reviews#create"
 end
