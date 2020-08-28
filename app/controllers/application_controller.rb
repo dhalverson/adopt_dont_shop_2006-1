06ca30c4a17a7258121934dce8b8f9266b0717a2
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-27 18:46:46 -0600

    Adds favorite pets session access and forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # attr_reader :
+  protect_from_forgery with: :exception
+  helper_method :favorite_pets
+
+  def favorite_pets
+    @favorite_pets ||= Favorites.new(session[:favorites])
+  end
 end
