0ca945f895150b50f680ed70008ba8a40367d84d
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-29 14:08:48 -0600

    Remove previous user story text

--- spec/features/favorites/new_spec.rb
+++ spec/features/favorites/new_spec.rb
@@ -1,12 +1,3 @@
-# User Story 9, Favorite Creation
-#
-# As a visitor
-# When I visit a pet's show page
-# I see a button or link to favorite that pet
-# When I click the button or link
-# I'm taken back to that pet's show page
-# I see a flash message indicating that the pet has been added to my favorites list
-# The favorite indicator in the nav bar has incremented by one
 require 'rails_helper'
 
 RSpec.describe "As a visitor", type: :feature do
