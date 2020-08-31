6c11de971fcc673a45630840058ebd51829e5167
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-30 19:44:16 -0600

    Adds create and new actions, params, validate pet ids, and update sess

--- app/controllers/adoption_applications_controller.rb
+++ app/controllers/adoption_applications_controller.rb
@@ -1,11 +1,39 @@
 class AdoptionApplicationsController < ApplicationController
 
   def new
+    @favorites = favorite_pets.details
   end
 
   def create
-    require "pry"; binding.pry
-    redirect_to "/favorites"
+    adoption_application = AdoptionApplication.create(application_params)
+    if !adoption_application.save || !validate_pet_ids
+      flash[:error] = "Please complete all the fields to submit the application"
+      redirect_to "/adoption_applications/new"
+    elsif adoption_application.save && validate_pet_ids
+      flash[:success] = "Application succesfully submitted for selected pets"
+      update_session(params[:pet_ids])
+      redirect_to "/favorites"
+    end
+  end
+
+
+private
+
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :phone, :description)
+  end
+
+  def validate_pet_ids
+    return false if params[:pet_ids].nil? || params[:pet_ids].empty?
+    params[:pet_ids].all? do |pet_id|
+      Pet.exists?(pet_id)
+    end
+  end
+
+  def update_session(pet_ids)
+    pet_ids.each do |pet_id|
+      session[:favorites].delete(pet_id)
+    end
   end
 
 end
