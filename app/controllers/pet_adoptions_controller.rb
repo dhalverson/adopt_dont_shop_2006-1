28e1616f106c5396ae097ee6a71ae61e93013965
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-01 15:32:09 -0600

    Adds update action and content

--- app/controllers/pet_adoptions_controller.rb
+++ app/controllers/pet_adoptions_controller.rb
@@ -1,7 +1,15 @@
 class PetAdoptionsController < ApplicationController
-  
+
   def index
     @pet = Pet.find(params[:id])
   end
-  
+
+  def update
+    @pet = Pet.find(params[:pet_id])
+    @pet_adoption = PetAdoption.find(params[:pet_adoption_id])
+    @pet_adoption.update(status: "Pending")
+    @pet.update(status: "Pending")
+    redirect_to "/pets/#{params[:pet_id]}"
+  end
+
 end
