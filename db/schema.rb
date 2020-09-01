2826e5339424e3ca60d2c17196a2fc8d2ace94c1
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-01 15:34:23 -0600

    Adds additional column status to pet adoptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_023651) do
+ActiveRecord::Schema.define(version: 2020_09_01_203807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_023651) do
     t.bigint "adoption_application_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "Adoptable"
     t.index ["adoption_application_id"], name: "index_pet_adoptions_on_adoption_application_id"
     t.index ["pet_id"], name: "index_pet_adoptions_on_pet_id"
   end
