45d18eb011382b8d0006f91293024e2e80398c9c
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-02 14:07:44 -0600

    Minor changes

--- app/models/shelter_review.rb
+++ app/models/shelter_review.rb
@@ -4,4 +4,12 @@ class ShelterReview < ApplicationRecord
   validates_presence_of :title,
                         :rating,
                         :content
+                        
+  # def self.average(reviews)
+  #   total = 0
+  #   reviews.each do |review|
+  #     total += review.rating.to_i
+  #   end
+  #   total / reviews.size
+  # end
 end
