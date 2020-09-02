b0ddbb13fabe437b912eebf1ccbcd522091d05b1
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-01 21:06:51 -0600

    Adjust logic

--- app/controllers/pet_adoptions_controller.rb
+++ app/controllers/pet_adoptions_controller.rb
@@ -7,15 +7,18 @@ class PetAdoptionsController < ApplicationController
   def update
     @pet_adoption = PetAdoption.find(params[:pet_adoption_id])
     @pet = Pet.find(params[:pet_id])
-    if @pet.status == "Pending"
+    if @pet.status == "Pending" && @pet_adoption.status == "Pending"
       @pet_adoption.update(status: "Adoptable")
       @pet.update(status: "Adoptable")
       redirect_to "/applications/#{@pet_adoption.adoption_application_id}"
-    else
+    elsif @pet.status == "Adoptable" && @pet_adoption.status == "Adoptable"
       @pet_adoption = PetAdoption.find(params[:pet_adoption_id])
       @pet_adoption.update(status: "Pending")
       @pet.update(status: "Pending")
       redirect_to "/pets/#{params[:pet_id]}"
+    else
+      flash[:error] = "Error, this pet has a pending application"
+      redirect_to "/applications/#{@pet_adoption.adoption_application_id}"
     end
   end
 
