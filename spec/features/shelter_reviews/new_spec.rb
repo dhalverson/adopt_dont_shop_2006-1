0a6b25e5595167943e5ff3b84429fb8be5c2a10e
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-02 14:07:26 -0600

    Edit tests

--- spec/features/shelter_reviews/new_spec.rb
+++ spec/features/shelter_reviews/new_spec.rb
@@ -51,23 +51,3 @@ RSpec.describe "As a visitor", type: :feature do
     expect(current_path).to eq("/shelters/#{@shelter_1.id}/reviews/new")
   end
 end
-    # User Story 3, Shelter Review Creation
-    #
-    # As a visitor,
-    # When I visit a shelter's show page
-    # I see a link to add a new review for this shelter.
-    # When I click on this link, I am taken to a new review path
-    # On this new page, I see a form where I must enter:
-    # - title
-    # - rating
-    # - content
-    # I also see a field where I can enter an optional image (web address)
-    # When the form is submitted, I should return to that shelter's show page
-    # and I can see my new review
-
-    # User Story 4, Shelter Review Creation, cont.
-    #
-    # As a visitor,
-    # When I fail to enter a title, a rating, and/or content in the new shelter review form, but still try to submit the form
-    # I see a flash message indicating that I need to fill in a title, rating, and content in order to submit a shelter review
-    # And I'm returned to the new form to create a new review
